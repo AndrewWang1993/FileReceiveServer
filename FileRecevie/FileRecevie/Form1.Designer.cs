a9b7d22ad9cfb66bb744bd1d3055ecfe0d482a11
PoeceWang <matthew98981@gmail.com>
2014-11-24 11:18:21 +0800

    C#图片自动推送服务器

---
+++ FileRecevie/FileRecevie/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace FileRecevie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.richTextBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(-2, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(519, 299);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "图片接收服务器";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 293);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "图片接收服务器";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ FileRecevie/FileRecevie/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FileRecevie
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        Socket client;
+        public Form1()
+        {
+            InitializeComponent();
+            Thread TempThread = new Thread(new ThreadStart(this.StartReceive));
+            TempThread.Start();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void StartReceive()
+        {
+            while (true)
+            {
+
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(GetLocalIp()), 17000);
+
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                server.Bind(ipep);
+
+                server.Listen(10);
+
+                client = server.Accept();
+
+                //       IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+
+                string SendFileName = System.Text.Encoding.Unicode.GetString(TransferFiles.ReceiveVarData(client));
+
+                string path = Environment.CurrentDirectory+"/.."+"/../pic";
+                //     string BagSize = System.Text.Encoding.Unicode.GetString(TransferFiles.ReceiveVarData(client));
+
+                //      int bagCount = int.Parse(System.Text.Encoding.Unicode.GetString(TransferFiles.ReceiveVarData(client)));
+
+                //      string bagLast = System.Text.Encoding.Unicode.GetString(TransferFiles.ReceiveVarData(client));
+
+                string fileaddr = path + "//" + SendFileName;
+
+                FileStream MyFileStream = new FileStream(fileaddr, FileMode.Create, FileAccess.Write);
+
+                //        int SendedCount = 0;
+
+                while (true)
+                {
+                    byte[] data = TransferFiles.ReceiveVarData(client);
+                    if (data.Length == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        // SendedCount++;
+                        MyFileStream.Write(data, 0, data.Length);
+                    }
+                }
+                richTextBox1.AppendText(fileaddr + " Created   \n");
+                MyFileStream.Close();
+                server.Close();
+                client.Close();
+
+
+            }
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if (server != null)
+            //{
+            //    server.Close();
+            //}
+            //if (client != null)
+            //{
+            //    client.Close();
+            //}
+            System.Environment.Exit(0);
+
+
+
+        }
+        private string GetLocalIp()
+        {
+            string strHostIP = "";
+            IPHostEntry oIPHost = Dns.Resolve(Environment.MachineName);
+            if (oIPHost.AddressList.Length > 0)
+                strHostIP = oIPHost.AddressList[0].ToString();
+            return strHostIP;
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.Activate();
+            }
+        }
+    }
+}
